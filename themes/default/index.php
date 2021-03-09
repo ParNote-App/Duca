b7e8b7143be63ee4f7fd6faae894dbf4047201fa
kahverengi <ahmet.duruer@yandex.com>
2021-03-09 19:22:07 +0300

    Separate index into header and footer

--- themes/default/index.php
+++ themes/default/index.php
@@ -1,15 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8"/>
-    <meta content="width=device-width,initial-scale=1" name="viewport"/>
+<?php
 
-    <title>DUCA - A portfolio PHP script.</title>
-    <link rel="stylesheet" href="/assets/css/style.css">
-</head>
-<body>
-<div>
-    Hello world from DUCA!
-</div>
-</body>
-</html>
+global $config;
+
+require_once "template/header.php";
+?>
+
+    <div>
+        Hello world from DUCA!
+    </div>
+
+<?php
+
+require_once "template/footer.php";
---
+++ themes/default/template/header.php
@@ -0,0 +1,14 @@
+<?php
+global $config;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <meta content="width=device-width,initial-scale=1" name="viewport"/>
+
+    <title></title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+<body>
