0a4fb7e4483cbc626a80d06f894acd486b9af769
alphazarc <alphazarc@gmail.com>
2021-03-27 16:59:32 +0300

    Add Error404 page

--- themes/default/error404.php
+++ themes/default/error404.php
@@ -1,1 +1,12 @@
-<h1>Error 404</h1>
+<?php
+
+global $config;
+
+include 'template/header.php'
+?>
+    <main>
+        <div class="text-center error404">
+            <h1>Error 404</h1>
+        </div>
+    </main>
+<?php include 'template/footer.php';
