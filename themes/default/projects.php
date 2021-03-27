48bfa0cdfab7232f1e80bfa21ad8a667a95d66aa
alphazarc <alphazarc@gmail.com>
2021-03-27 17:41:52 +0300

    Add php logics to projects.php

--- themes/default/projects.php
+++ themes/default/projects.php
@@ -6,9 +6,23 @@ include 'template/header.php'
 ?>
 
 <div class="project-parnote">
-  <a target="_blank" href="https://github.com/ParNote-App"
-    ><img src="/assets/img/parnote.png" alt="project-parnote"
-  /></a>
+    <?php
+    foreach ($config["projects"] as $project) {
+        ?>
+        <p>
+            <a
+                    href="<?= $project['link'] ?>"
+                    target="_blank">
+                ᐅ <?= $project['title'] ?>:
+            </a>
+
+            <?= $project['description'] ?>
+        </p>
+
+        <?php
+    }
+    ?>
+
 </div>
 
 <?php include 'template/footer.php';
